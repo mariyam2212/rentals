119feab2ebd73991638efe3c1bb1a9b8dfa73efb
Fatima Mariyam <fatimamariyam@Fatimas-MacBook-Air.local>
2023-04-29 13:32:00 -0500

    address_info model added

---
+++ rentalapp/src/main/java/model/Address_Info.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Address_Info {
+    private int address_id;
+    private String street;
+    private String city;
+    private String country;
+    private String zip_code;
+
+    public int getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+}
--- rentalapp/src/main/java/model/Property.java
+++ rentalapp/src/main/java/model/Property.java
@@ -1,7 +1,7 @@
 package model;
 
 
-public class Property {
+public class Property implements BaseModel{
     private int property_id;
     private String property_name;
     private int total_units;
