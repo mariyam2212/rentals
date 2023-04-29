2a84f180822fce987f47548f5e1fb4e4dd4ec222
spandanavemula7 <spandanavemula7@gmail.com>
2023-04-29 15:03:08 -0500

    booking_info and payment_info tables
    booking_info and payment_info tables

---
+++ rentalapp/src/main/java/model/Booking_Info.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Date;
+
+public class Booking_Info {
+    private int booking_id;
+    private int renter_id;
+    private Date start_date;
+    private Date end_date;
+    private int booking_unit_id;
+    private String booking_status;
+    private int transaction_id;
+
+    public int getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(int booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public int getRenter_id() {
+        return renter_id;
+    }
+
+    public void setRenter_id(int renter_id) {
+        this.renter_id = renter_id;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public int getBooking_unit_id() {
+        return booking_unit_id;
+    }
+
+    public void setBooking_unit_id(int booking_unit_id) {
+        this.booking_unit_id = booking_unit_id;
+    }
+
+    public String getBooking_status() {
+        return booking_status;
+    }
+
+    public void setBooking_status(String booking_status) {
+        this.booking_status = booking_status;
+    }
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+}
