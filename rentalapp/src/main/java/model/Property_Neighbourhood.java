8b0875d4cd1285dc66a1ff2be3693cee94799993
spandanavemula7 <spandanavemula7@gmail.com>
2023-04-29 15:46:02 -0500

    Property_neighbourhood, transaction_info, unit_features,unit_info, user_type_info tables
    Property_neighbourhood, transaction_info, unit_features,unit_info, user_type_info tables

---
+++ rentalapp/src/main/java/model/Property_Neighbourhood.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Property_Neighbourhood {
+private int property_id;
+private Float nearest_grocery;
+private Float nearest_school;
+private Float nearest_bus_stop;
+private Float nearest_metro;
+private Float nearest_atm;
+private Float nearest_airport;
+
+    public int getProperty_id() {
+        return property_id;
+    }
+
+    public void setProperty_id(int property_id) {
+        this.property_id = property_id;
+    }
+
+    public Float getNearest_grocery() {
+        return nearest_grocery;
+    }
+
+    public void setNearest_grocery(Float nearest_grocery) {
+        this.nearest_grocery = nearest_grocery;
+    }
+
+    public Float getNearest_school() {
+        return nearest_school;
+    }
+
+    public void setNearest_school(Float nearest_school) {
+        this.nearest_school = nearest_school;
+    }
+
+    public Float getNearest_bus_stop() {
+        return nearest_bus_stop;
+    }
+
+    public void setNearest_bus_stop(Float nearest_bus_stop) {
+        this.nearest_bus_stop = nearest_bus_stop;
+    }
+
+    public Float getNearest_metro() {
+        return nearest_metro;
+    }
+
+    public void setNearest_metro(Float nearest_metro) {
+        this.nearest_metro = nearest_metro;
+    }
+
+    public Float getNearest_atm() {
+        return nearest_atm;
+    }
+
+    public void setNearest_atm(Float nearest_atm) {
+        this.nearest_atm = nearest_atm;
+    }
+
+    public Float getNearest_airport() {
+        return nearest_airport;
+    }
+
+    public void setNearest_airport(Float nearest_airport) {
+        this.nearest_airport = nearest_airport;
+    }
+}
---
+++ rentalapp/src/main/java/model/Transaction_Info.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.util.Date;
+
+public class Transaction_Info {
+    private int transaction_id;
+    private float txn_amount;
+    private String card_number;
+    private int address_id;
+    private int payment_type_id;
+    private Date txn_date;
+    private String txn_status;
+    private Date expiry_date;
+
+    public int getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(int transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public float getTxn_amount() {
+        return txn_amount;
+    }
+
+    public void setTxn_amount(float txn_amount) {
+        this.txn_amount = txn_amount;
+    }
+
+    public String getCard_number() {
+        return card_number;
+    }
+
+    public void setCard_number(String card_number) {
+        this.card_number = card_number;
+    }
+
+    public int getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+
+    public int getPayment_type_id() {
+        return payment_type_id;
+    }
+
+    public void setPayment_type_id(int payment_type_id) {
+        this.payment_type_id = payment_type_id;
+    }
+
+    public Date getTxn_date() {
+        return txn_date;
+    }
+
+    public void setTxn_date(Date txn_date) {
+        this.txn_date = txn_date;
+    }
+
+    public String getTxn_status() {
+        return txn_status;
+    }
+
+    public void setTxn_status(String txn_status) {
+        this.txn_status = txn_status;
+    }
+
+    public Date getExpiry_date() {
+        return expiry_date;
+    }
+
+    public void setExpiry_date(Date expiry_date) {
+        this.expiry_date = expiry_date;
+    }
+}
